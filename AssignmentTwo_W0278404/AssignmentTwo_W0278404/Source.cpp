0868a857b6c58a0b7dbc946117529f3899e2d397
ShaneTibert <Shane.J.Tibert@gmail.com>
2016-02-15 23:36:17 -0400

    Assignment #2: Display and data grab complete

--- AssignmentTwo_W0278404/AssignmentTwo_W0278404/Source.cpp
+++ AssignmentTwo_W0278404/AssignmentTwo_W0278404/Source.cpp
@@ -5,88 +5,37 @@
 
 using namespace std;
 
-int sel = 6;
-int val[7];
+int sel = 0;
+int lines = 0;
 
-char** filename;
+char** filename = new char *[255];
+char** maze = new char *[255];
 
-bool saveflag = false;
-bool run = true;
-
-List combo;
-
-void display();
-void commands(char arg, int val);
+void mainDisplay();
+void readFromFile(char**);
 void help();
 
 int main(int argc, char** argv) {
 	if (argc > 1) {
-		filename[0] = argv[0];
+		readFromFile(argv);
 	}
-	char** filename = argv;
-	sel = 1;
 
-	char input[4];
-	char cmd;
-	char** filename = argv;
+	mainDisplay();
+	_getch();
+	return 0;
 
-	int val = 0;
-
-	display();
-
-}
-
-
-
-void commands(char arg, int val)
-{
 }
 
 void mainDisplay()
 {
 	system("cls");
-
-	for (int i = 0; i < 7; i++)
-	{
-		val[i] = combo.RetrievePosition(i + 1);
+	for (int i = 0; i < lines; i++) {
+		for (int c=0; c < sel; c++) {
+			cout << maze[i][c];
+		}
+		cout << endl;
 	}
 
-	cout << (char)218 << (char)196 << (char)196 << (char)196 << (char)196 << (char)196 << (char)196 << (char)196 << (char)196 << (char)196 << (char)196 << (char)196 << (char)191 << endl;
-	cout << (char)179 << "                                        " << (char)179 << endl;
-	cout << (char)179 << "                                        " << (char)179 << endl;
-	cout << (char)179 << "                                        " << (char)179 << endl;
-	cout << (char)179 << "                                        " << (char)179 << endl;
-	cout << (char)179 << "                                        " << (char)179 << endl;
-
-	// chunks through a for loop printing Rs on odd interations and Ls on evens
-	for (int i = 0; i < 7; i++) {
-		if (i & 1) {
-			if (sel == i) {
-				cout << ">" << "R" << val[i] << "<";
-			}
-			else cout << "R" << val[i];
-
-		}
-		else {
-			if (sel == i) {
-				cout << ">" << "L" << val[i] << "<";
-			}
-			else cout << "L" << val[i];
-		}
-	}
-	cout << (char)179 << "                                        " << (char)179 << endl;
-	cout << (char)179 << "                                        " << (char)179 << endl;
-	cout << (char)179 << "                                        " << (char)179 << endl;
-	cout << (char)179 << "                                        " << (char)179 << endl;
-	cout << (char)179 << "                                        " << (char)179 << endl;
-}
-
-void helpScreen()
-{
-	cout << "D = Delete | R = Reset | E = Exit And Save" << endl;
-	cout << "G = Goto | S = Subsitute | Q = Quit And Do Not Save" << endl;
-
-	_getch();
 }
 
 void readFromFile(char** filename) {
@@ -96,33 +45,33 @@ void readFromFile(char** filename) {
 	try {
 		if (filename[1] != nullptr)
 		{
-			read.open("");
-
-			char 1 = '+';
-
-			int line = 0;
-			int point = 0;
-
+			read.open(filename[1]);
+			char i;
+			
 			if (read.is_open()) {
-
+				char i = ' ';
+				char *vals = new char[255];
 				while (!read.eof()) {
-					char *vals = new char[255];
-					char i = ' ';
-
-					while (i != '\n') {
-						if (!read.eof()) {
-							i = read.get();
-							if (1 != -1) {
-							}
-						}
+					i = read.get();
+					if (i == '\n') {
+						vals[sel] = '\0';
+						maze[lines] = vals;
+						lines++;
+						vals = new char[255];
+						sel = 0;
 					}
+					else { vals[sel] = i; sel++; }
 				}
+				maze[lines] = vals;
+				lines++;
 			}
+			
 		}
 	}
 	catch (exception e) {
 		cout << e.what();
 	}
+	read.close();
 }
 
 		/*dataForFile.open(filename);
